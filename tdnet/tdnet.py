f4d77b80e061a0ea76a7d770d890e6fbfaecd134
masato <masa.5tonic@gmail.com>
2019-04-14 13:59:50 +0900

    roughly fixed the parser problem

--- tdnet/tdnet.py
+++ tdnet/tdnet.py
@@ -2,7 +2,10 @@ import urllib.request
 import urllib.parse
 import datetime
 import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import XMLPullParser
 import xml.dom.minidom as minidom
+
+from bs4 import BeautifulSoup
 
 URL = 'https://www.release.tdnet.info/onsf/TDJFSearch/TDJFSearch'
 HEADERS = {
@@ -14,7 +17,17 @@ HEADERS = {
 START_DATE_KEY = 't0'
 END_DATE_KEY = 't1'
 Q_KEY = 'q'
-DATE_FORMAT = '%Y-%m-%d'
+DATE_FORMAT = '%Y%m%d'
+
+TD_SELECTOR_TIME = 'td.time'
+TD_SELECTOR_CODE = 'td.code'
+TD_SELECTOR_COMPANY_NAME = 'td.companyname'
+TD_SELECTOR_TITLE = 'td.title'
+TD_SELECTOR_XBRL = 'td.xbrl'
+TD_SELECTOR_EXCHANGE = 'td.exchange'
+TD_SELECTOR_UPDATE = 'td.update'
+
+MAIN_TABLE_SELECTOR = '#maintable'
 
 def search(start_date, end_date, q):
     if not start_date or not end_date:
@@ -34,9 +47,20 @@ def search(start_date, end_date, q):
     request = urllib.request.Request(URL, data, HEADERS)
     response = urllib.request.urlopen(request)
     body = str(response.read())
-    print(body)
-    root = minidom.parseString(body)
-    #print(root)
+    _parse(body)
+
+def _parse(body):
+    soup = BeautifulSoup(body, features='html.parser')
+    soup_result = soup.select(MAIN_TABLE_SELECTOR)
+    if len(soup_result) != 0:
+        table_element = soup_result[0]
+        for row_element in table_element.find_all('tr'):
+            print(row_element.select(TD_SELECTOR_TIME)[0].get_text())
+            print(row_element.select(TD_SELECTOR_CODE)[0].get_text())
+            print(row_element.select(TD_SELECTOR_COMPANY_NAME)[0].get_text())
+            print(row_element.select(TD_SELECTOR_EXCHANGE)[0].get_text())
+            print(row_element.select(TD_SELECTOR_UPDATE)[0].get_text())
+
 
 if __name__ == '__main__':
     date = datetime.datetime(2019, 4, 3)
